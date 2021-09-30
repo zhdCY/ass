d9f91a07ddbe0e72ef7dc13d2e57413a35646a29
Li kexin <963799688@qq.com>
2021-09-30 12:34:02 +0000

    update src/main/java/assignment1/text_editor.java.

--- src/main/java/assignment1/text_editor.java
+++ src/main/java/assignment1/text_editor.java
@@ -29,7 +29,9 @@ import java.util.Date;
 
 public class text_editor extends Application {
     private Stage primaryStage;
-
+    private String path=null;
+    boolean save = false;
+    int startindex=0;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -118,12 +120,66 @@ public class text_editor extends Application {
 
     @FXML
     void NewFile(ActionEvent event) {
+        if (save){
+            textarea.setText("");
+            primaryStage.setTitle("new file"+"-Text Editor");
+            path=null;
+        }else if (!save&&!textarea.getText().isEmpty()){
+            Stage stage1=new Stage();
+            VBox layout1=new VBox();
+            layout1.setSpacing(5);
+            Label label1=new Label("You don't save current file!");
+            layout1.getChildren().addAll(label1);
+            Button yes=new Button("Yes");
+            Button cancel=new Button("Cancel");
+            HBox layout2=new HBox();
+            layout2.setSpacing(10);
+            layout2.getChildren().addAll(yes,cancel);
+            layout1.setAlignment(Pos.CENTER);
+            layout2.setAlignment(Pos.CENTER);
+            BorderPane pane=new BorderPane();
+            pane.setTop(layout1);
+            pane.setCenter(layout2);
+            Scene scene=new Scene(pane,300,150);
+            stage1.setTitle("Warning!");
+            stage1.setScene(scene);
+            stage1.show();
+            yes.setOnAction(event1 -> {
+                stage1.close();
+                textarea.setText("");
+                primaryStage.setTitle("new file"+"-Text Editor");
+                path=null;
 
+            });
+            cancel.setOnAction(event1 -> {
+                stage1.close();
+            });
+        }
     }
 
     @FXML
     void OpenFile(ActionEvent event) {
-
+        FileChooser fileChooser=new FileChooser();
+        fileChooser.setTitle("Choose a file and open");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("TXT","*.txt"));
+        File file=fileChooser.showOpenDialog(primaryStage);
+        if (file!=null&&file.exists()){
+            try{
+                FileInputStream read=new FileInputStream(file);
+                byte[] m=new byte[(int) file.length()];
+                read.read(m);
+                textarea.setText(new String(m));
+                read.close();
+                path=file.getPath();
+                int lasttext;
+                String t;
+                lasttext=path.lastIndexOf("\\");
+                t=path.substring(lasttext+1);
+                primaryStage.setTitle(t+"-Text Editor");
+            }catch (Exception e){
+//                e.printStackTrace();
+            }
+        }
     }
 
     @FXML
